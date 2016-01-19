6f7200e92dabcd64dc5108c66f7a85e57d8ca690
Tom Gray <tgray@harwoodandrews.com.au>
2016-01-19 21:52:23 +1100

    formatting

--- controllers/employee.js
+++ controllers/employee.js
@@ -2,63 +2,63 @@ var db = require("../core/db");
 var httpMsgs = require("../core/httpMessages");
 var util = require("util");
 
-exports.getList = function(req, res) {
-    db.executeSQL("SELECT * FROM status", function(data, err) {
-       if(err) {
-           httpMsgs.show500(req, res, err);
-       } 
-       else {
-           res.writeHeader(200, {"Content-type": "application/json"});
-           res.write(JSON.stringify(data)); //use modular version
+exports.getList = function (req, res) {
+    db.executeSQL("SELECT * FROM status", function (data, err) {
+        if (err) {
+            httpMsgs.show500(req, res, err);
+        }
+        else {
+            res.writeHeader(200, { "Content-type": "application/json" });
+            res.write(JSON.stringify(data)); //use modular version
            
-       }
-       res.end(); // res.end goes here now
+        }
+        res.end(); // res.end goes here now
     });
 };
 
-exports.get = function(req, res, empno){
-     db.executeSQL("SELECT * FROM status WHERE lawyer=" + empno, function(data, err) {
-       if(err) {
-           httpMsgs.show500(req, res, err);
-       } 
-       else {
-           res.writeHeader(200, {"Content-type": "application/json"});
-           res.write(JSON.stringify(data)); //use modular version
+exports.get = function (req, res, empno) {
+    db.executeSQL("SELECT * FROM status WHERE lawyer=" + empno, function (data, err) {
+        if (err) {
+            httpMsgs.show500(req, res, err);
+        }
+        else {
+            res.writeHeader(200, { "Content-type": "application/json" });
+            res.write(JSON.stringify(data)); //use modular version
            
-       }
-       res.end(); // res.end goes here now
+        }
+        res.end(); // res.end goes here now
     });
 };
 
 exports.add = function (req, res, reqBody) {
     try {
-        if(!reqBody) throw new Error("Input not valid");
+        if (!reqBody) throw new Error("Input not valid");
         var data = JSON.parse(reqBody);
-        if(data) {
+        if (data) {
             var sql = "INSERT INTO status (Lawyer, Matter, Client, Description, Status) VALUES";
-            sql+= util.format("(%s, %s, %s, %s, %s, %s)", data.Lawyer, data.Matter, data.client, data.description, data.status);
+            sql += util.format("(%s, %s, %s, %s, %s, %s)", data.Lawyer, data.Matter, data.client, data.description, data.status);
             db.executeSQL(sql, function (data, err) {
-            if(err) {
-                httpMsgs.show500(req, res, err);
-            } 
-            else {
-                httpMsgs.send200(req, res);
-            }
-    });
+                if (err) {
+                    httpMsgs.show500(req, res, err);
+                }
+                else {
+                    httpMsgs.send200(req, res);
+                }
+            });
         }
-        else { 
+        else {
             throw new Error("Input not valid");
         }
     }
-    catch (ex){
+    catch (ex) {
         httpMsgs.show500(req, res, ex);
     }
 };
 
-exports.update  = function (req, res, reqBody) {
-    
+exports.update = function (req, res, reqBody) {
+
 };
 
-exports.delete  = function (req, res, reqBody) {
-    
+exports.delete = function (req, res, reqBody) {
+
 };

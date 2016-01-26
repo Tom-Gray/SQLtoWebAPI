8e806f75404af1f3f2d4cff376ef035f155cfe6c
Tom Gray <tgray@harwoodandrews.com.au>
2016-01-27 08:09:05 +1100

    adding code for using GET from slack

--- controllers/slackrtm.js
+++ controllers/slackrtm.js
@@ -1,5 +1,5 @@
 //
-
+var emp = require('../controllers/employee');
 var token = require('../token');
 var Botkit = require('Botkit');
 
@@ -8,14 +8,20 @@ var controller = Botkit.slackbot({
     debug: false,
 });
 
-var pattern = "[M\d\d\d\d\d\d\d\d]"; //regex to validate shit
-                var patt = new RegExp(pattern);
+var pattern = "[M\\d{8}]"; //regex to validate matter record
+                
 
 var bot = controller.spawn(token.tokenstring).startRTM();
     
-    controller.hears(patt, 'direct_message,direct_mention,mention', function (bot, message) {
+    controller.hears(pattern, 'direct_message,direct_mention,mention', function (bot, message) {
+        //send request with matter number to SQL GET command
+        var empno = "";
+        empno = message.text //validate this first
+         emp.get(empno);
+         
+         
     bot.startConversation(message, function (err, convo) {
-        convo.say('Heard ya');
+        convo.say('you requested Matter: ' + message.text);
     });
 
     bot.startPrivateConversation(message, function (err, dm) {

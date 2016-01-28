c0a294583cff99923139924638954f78693fc571
Tom Gray <tgray@harwoodandrews.com.au>
2016-01-28 21:40:59 +1100

    initial slackbot conversation

--- controllers/slackrtm.js
+++ controllers/slackrtm.js
@@ -15,13 +15,14 @@ var bot = controller.spawn(token.tokenstring).startRTM();
     
     controller.hears(pattern, 'direct_message,direct_mention,mention', function (bot, message) {
         //send request with matter number to SQL GET command
+        //initiate http request from here?
         var empno = "";
         empno = message.text //validate this first
          emp.get(empno);
          
          
     bot.startConversation(message, function (err, convo) {
-        convo.say('you requested Matter: ' + message.text);
+        convo.say('Whats your update for matter ' + message.text);
     });
 
     bot.startPrivateConversation(message, function (err, dm) {
@@ -29,3 +30,32 @@ var bot = controller.spawn(token.tokenstring).startRTM();
     })
 
 });
+
+controller.hears(['update'],['ambient'],function(bot,message) {
+  bot.startConversation(message, askFlavor);
+});
+
+askFlavor = function(response, convo) {
+  convo.ask("What Matter do you want to update?", function(response, convo) {
+    convo.say("Awesome.");
+    varMatter = response.text
+    askSize(response, convo);
+    convo.next();
+  });
+}
+askSize = function(response, convo) {
+  convo.ask("Whats the latest on matter " + varMatter + "?", function(response, convo) {
+    //convo.say("Ok.")
+    varStatus = response.text
+    askWhereDeliver(response, convo);
+    convo.next();
+  });
+}
+askWhereDeliver = function(response, convo) { 
+  convo.ask("Great. I'll record that matter " + varMatter + " is " + varStatus, function(response, convo) {
+    //convo.say("Good bye.");
+    convo.next();
+  });
+}
+
+

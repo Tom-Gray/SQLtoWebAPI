c812364bfc7bd2f77f6f376cb92abcc884943b8b
Tom Gray <tgray@harwoodandrews.com.au>
2016-01-30 09:35:05 +1100

    changes to frontend layout

---
+++ frontend/app.js
@@ -0,0 +1,6 @@
+ var countryApp = angular.module('statusApp', []);
+      countryApp.controller('CountryCtrl', function ($scope, $http){
+        $http.get('http://localhost:9000/employees/').success(function(data) {
+          $scope.countries = data;
+        });
+      });

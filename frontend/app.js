6343238563acac5b21bcc8d4f670f240e3c70746
Tom Gray <tgray@harwoodandrews.com.au>
2016-01-30 19:50:06 +1100

    moved angular to app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
- var countryApp = angular.module('statusApp', []);
+var countryApp = angular.module('statusApp', []);
       countryApp.controller('CountryCtrl', function ($scope, $http){
         $http.get('http://localhost:9000/employees/').success(function(data) {
           $scope.countries = data;

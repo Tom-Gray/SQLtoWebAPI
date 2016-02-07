5fdf049780f0962360a4b61aa8a12a43f2cd4a32
Tom Gray <tgray@harwoodandrews.com.au>
2016-02-07 14:03:49 +1100

    More convoersation cleanup

--- controllers/slackrtm.js
+++ controllers/slackrtm.js
@@ -47,7 +47,11 @@ controller.hears(pattern, 'direct_message,direct_mention,mention', function (bot
 
         bot.startConversation(message, function (err, convo) {
             convo.say('```Client: ' + recordClient + "\nMatter: " + recordDescription + "\nCurrent Status: " + recordStatus + "```");
-            convo.say('Whats the latest status for this matter?')
+            convo.ask('Whats the latest status for this matter?', function(response, convo){
+                varMatter = response.text;
+                convo.say("No worries. The status of *" + recordMatter + "* is now " + varMatter);
+                convo.next();
+            })
         });
 
 
@@ -55,44 +59,4 @@ controller.hears(pattern, 'direct_message,direct_mention,mention', function (bot
 
 
 });
-
-
-
-
-
-controller.hears(['GoGoUpdate'], ['ambient'], function (bot, message) {
-    bot.startConversation(message, askFlavor);
-});
-
-askFlavor = function (response, convo) {
-    convo.ask("What Matter do you want to update?", function (response, convo) {
-        convo.say("Awesome.");
-        varMatter = response.text
-        askSize(response, convo);
-        convo.next();
-    });
-}
-askSize = function (response, convo) {
-    convo.ask("Whats the latest on matter " + varMatter + "?", function (response, convo) {
-        //convo.say("Ok.")
-        varStatus = response.text
-        askWhereDeliver(response, convo);
-        convo.next();
-    });
-}
-askWhereDeliver = function (response, convo) {
-    convo.ask("Great. I'll record that matter " + varMatter + " is " + varStatus, function (response, convo) {
-        //convo.say("Good bye.");
-        convo.next();
-    });
-}
- 
-
-
-
-
-
-
-
-//cnversation
 
